9cae187fcbc64ed62e54cb74fd43250281b42bbb
Alexandr-Minko <Minko.Alexandr@gmail.com>
2022-08-03 09:35:28 +0300

    Method connection to DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,41 +6,14 @@ import java.sql.SQLException;
 
 public class Util {
 
-        // Connect to MySQL
-        public static Connection getMySQLConnection() throws SQLException, ClassNotFoundException {
-            String hostName =   "localhost";
-            String dbName =     "test1";
-            String userName =   "root";
-            String password =   "12345@@@";
+    public Connection getDbConnection() throws SQLException {
+        String UserName =        "root";
+        String Password =        "12345@@@";
+        String ConnectionURL =   "jdbc:mysql://localhost:3306";
 
-            return getMySQLConnection(hostName, dbName, userName, password);
-        }
-
-        public static Connection getMySQLConnection(String hostName, String dbName,
-                                                    String userName, String password) throws SQLException,
-                ClassNotFoundException {
-            // Declare the class Driver for MySQL DB
-            // This is necessary with Java 5 (or older)
-            // Java6 (or newer) automatically find the appropriate driver.
-            // If you use Java> 5, then this line is not needed.
-            Class.forName("com.mysql.jdbc.Driver");
-
-            String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
-
-            Connection conn = DriverManager.getConnection(connectionURL, userName,
-                    password);
-            return conn;
-        }
-
-
-
-
-
-
-
-
-
-
-
-
+        Connection connection = DriverManager.getConnection(ConnectionURL, UserName, Password);
+        System.out.println("Подключение к БД: Успешно");
+        return connection;
+    }
+    
 }

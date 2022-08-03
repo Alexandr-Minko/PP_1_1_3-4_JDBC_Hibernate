3deb2a5359ce7444268448870bdff37fa64dc787
Alexandr-Minko <Minko.Alexandr@gmail.com>
2022-08-03 09:38:05 +0300

    Method connection to DB. Refactoring

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,15 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-
     public Connection getDbConnection() throws SQLException {
         String UserName =        "root";
         String Password =        "12345@@@";
         String ConnectionURL =   "jdbc:mysql://localhost:3306";
 
         Connection connection = DriverManager.getConnection(ConnectionURL, UserName, Password);
-        System.out.println("Подключение к БД: Успешно");
+        System.out.println("Подключение к БД: ОК");
         return connection;
     }
-    
 }

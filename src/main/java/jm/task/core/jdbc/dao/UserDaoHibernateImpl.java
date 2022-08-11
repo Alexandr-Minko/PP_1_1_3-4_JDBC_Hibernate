b1e01c845eb3ca37a1ed221e95fe668c2c4bde63
Alexandr-Minko <Minko.Alexandr@gmail.com>
2022-08-11 16:05:23 +0300

    hibernate completed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,133 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import javax.persistence.PersistenceException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
+
     public UserDaoHibernateImpl() {
-
+        sessionFactory = Util.getHibernateSessionFactory();
     }
-
 
     @Override
     public void createUsersTable() {
-
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE user (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), lastName VARCHAR(100), age TINYINT)").executeUpdate();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            try {
+                transaction.rollback();
+            } catch (IllegalStateException ex) {
+                System.out.println("Создание таблицы. Отмена изменений: Ошибка");
+            }
+        } finally {
+            if (session != null) {session.close();}
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE user").executeUpdate();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            try {
+                transaction.rollback();
+            } catch (IllegalStateException ex) {
+                System.out.println("Удаление таблицы. Отмена изменений: Ошибка");
+            }
+        } finally {
+            if (session != null) {session.close();}
+        }
     }
 
-    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(new User(name,lastName, age));
+            transaction.commit();
+        } catch (PersistenceException e) {
+            try {
+                transaction.rollback();
+            } catch (IllegalStateException ex) {
+                System.out.println("Добавить user-a по id. Отмена изменений: Ошибка");
+            }
+        } finally {
+            if (session != null) {session.close();}
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.createQuery("delete User where id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            try {
+                transaction.rollback();
+            } catch (IllegalStateException ex) {
+                System.out.println("Удалить user-a по id. Отмена изменений: Ошибка");
+            }
+        } finally {
+            if (session != null) {session.close();}
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> listUsers = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            listUsers = (List<User>) session.createQuery("From User").list();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            try {
+                transaction.rollback();
+            } catch (IllegalStateException ex) {
+                System.out.println("Получить всех user-ов. Отмена изменений: Ошибка");
+            }
+        } finally {
+            if (session != null) {session.close();}
+        }
+        return Objects.requireNonNullElseGet(listUsers, ArrayList::new);
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE user").executeUpdate();
+            session.createSQLQuery("CREATE TABLE user (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), lastName VARCHAR(100), age TINYINT)\"").executeUpdate();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            try {
+                transaction.rollback();
+            } catch (IllegalStateException ex) {
+                System.out.println("Очистка таблицы. Отмена изменений: Ошибка");
+            }
+        } finally {
+            if (session != null) {session.close();}
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,16 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static String userName =        "root";
-    private static String password =        "12345@@@";
-    private static String connectionURL =   "jdbc:mysql://localhost:3306/db1";
+    private static final String userName =        "root";
+    private static final String password =        "12345@@@";
+    private static final String connectionURL =   "jdbc:mysql://localhost:3306/db1";
 
     public static Connection getJDBCConnection() throws SQLException {
         return DriverManager.getConnection(connectionURL, userName, password);
     }
 
+    public static SessionFactory getHibernateSessionFactory() {
+        Configuration configuration = new Configuration()
+                .setProperty("hibernate.connection.url", connectionURL)
+                .setProperty("hibernate.connection.username", userName)
+                .setProperty("hibernate.connection.password", password)
+                .setProperty("show_sql", "true")
+                .setProperty("hibernate.format_sql", "true")
+                .addAnnotatedClass(User.class);
+
+        return configuration.buildSessionFactory();
+    }
 }
